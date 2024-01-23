42a2cb0ca9419dfc5a229f11ceae59fec076e786
Zheka <zheseleznev@yandex.ru>
2024-01-24 00:46:39 +0300

    Create profile simple page

--- app/(user)/profile/[userId]/page.tsx
+++ app/(user)/profile/[userId]/page.tsx
@@ -1,15 +1,30 @@
 'use client'
+import UserAvatar from '@/components/UserAvatar'
+import { Button } from '@/components/ui/button'
 import { Session } from 'next-auth'
 import { useSession } from 'next-auth/react'
+import { redirect, useRouter } from 'next/navigation'
 import React from 'react'
 
 function ProfilePage() {
   const { data: session } = useSession()
+  const router = useRouter()
   return (
-    <div>
-      <p>ID: {session?.user.id}</p>
-      <p>Name: {session?.user.name}</p>
-      <p>Email: {session?.user.email}</p>
+    <div className="flex justify-center items-center space-x-4 sm:flex-row flex-col">
+      <UserAvatar
+        name={session?.user.name}
+        image={session?.user.image}
+        imageWidth={100}
+        imageHeight={100}
+        className="w-20 h-20"
+      />
+      <div className="flex flex-col space-y-4 items-center justify-center">
+        <p className="font-medium">Name: {session?.user.name}</p>
+        <p className="font-medium">Email: {session?.user.email}</p>
+      </div>
+      <Button variant="default" onClick={() => router.push('/chat')}>
+        Go chat
+      </Button>
     </div>
   )
 }
--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -7,17 +7,25 @@ interface IUserAvatar {
   name?: string | null
   image?: string | null
   className?: string
+  imageWidth?: number
+  imageHeight?: number
 }
 
-const UserAvatar: FC<IUserAvatar> = ({ name, image, className }) => {
+const UserAvatar: FC<IUserAvatar> = ({
+  name,
+  image,
+  className,
+  imageWidth,
+  imageHeight,
+}) => {
   return (
     <Avatar className={cn('bg-white text-black', className)}>
       {image && (
         <Image
           src={image}
           alt={name || 'User name'}
-          width={40}
-          height={40}
+          width={imageWidth || 40}
+          height={imageHeight || 40}
           className="rounded-full"
         />
       )}
